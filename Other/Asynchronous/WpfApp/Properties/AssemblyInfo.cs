6611cda14ee56a060735b04738400395c4baa44f
daisuke nishino <daisuke.nishino.zg@hitachi-solutions.com>
2014-06-24 08:15:29 +0900

    ライセンスヘッダーの追加 (Add license header )

--- Other/Asynchronous/WpfApp/Properties/AssemblyInfo.cs
+++ Other/Asynchronous/WpfApp/Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using System.Reflection;
+﻿//**********************************************************************************
+//* Copyright (C) 2007,2014 Hitachi Solutions,Ltd.
+//**********************************************************************************
+
+#region Apache License
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -10,9 +30,9 @@ using System.Windows;
 [assembly: AssemblyTitle("WpfApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("seigi")]
+[assembly: AssemblyCompany("Hitachi Solutions")]
 [assembly: AssemblyProduct("WpfApp")]
-[assembly: AssemblyCopyright("Copyright © seigi 2010")]
+[assembly: AssemblyCopyright("Copyright (C) 2007,2014 Hitachi Solutions,Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Other/ThreadSafe/Properties/AssemblyInfo.cs
+++ Other/ThreadSafe/Properties/AssemblyInfo.cs
@@ -1,16 +1,36 @@
-﻿using System.Reflection;
+﻿//**********************************************************************************
+//* Copyright (C) 2007,2014 Hitachi Solutions,Ltd.
+//**********************************************************************************
+
+#region Apache License
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ThreadSafes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("seigi")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright © seigi 2008")]
+[assembly: AssemblyCompany("Hitachi Solutions")]
+[assembly: AssemblyProduct("ThreadSafe")]
+[assembly: AssemblyCopyright("Copyright (C) 2007,2014 Hitachi Solutions,Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
