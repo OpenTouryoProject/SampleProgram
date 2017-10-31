b007bec1bc2f7876ec5e9435436fcf944ba25194
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2017-10-31 11:39:19 +0900

    Add new project that is as the template of Cordova.

---
+++ Template/OtrCdvTpl/OtrCdvTpl/merges/windows/scripts/platformOverrides.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    // safeHTML ポリフィルを追加します
+    var scriptElem = document.createElement('script');
+    scriptElem.setAttribute('src', 'scripts/winstore-jscompat.js');
+    if (document.body) {
+        document.body.appendChild(scriptElem);
+    } else {
+        document.head.appendChild(scriptElem);
+    }
+}());
---
+++ Template/OtrCdvTpl/OtrCdvTpl/www/scripts/index.js
@@ -0,0 +1,30 @@
+﻿// 空白のテンプレートの概要については、次のドキュメントを参照してください:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// ページ上のコードをデバッグするには、Ripple で読み込むか、Android デバイス/エミュレーターで読み込みます。アプリを起動し、ブレークポイントを設定します。
+// 次に、JavaScript コンソールで "window.location.reload()" を実行します。
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Cordova の一時停止を処理し、イベントを再開します
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova が読み込まれました。ここで、Cordova を必要とする初期化を実行します。
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: このアプリケーションは中断されました。ここで、アプリケーションの状態を保存します。
+    };
+
+    function onResume() {
+        // TODO: このアプリケーションが再アクティブ化されました。ここで、アプリケーションの状態を復元します。
+    };
+} )();
