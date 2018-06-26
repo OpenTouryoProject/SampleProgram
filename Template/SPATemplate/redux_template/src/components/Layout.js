fa3b256599016db9142262dd770ae0ba97f310be
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-06-26 16:43:34 +0900

    Added the redux_template.

---
+++ Template/SPATemplate/redux_template/src/components/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import { NavMenu } from './NavMenu';
+import logo from './logo.svg';
+import './Layout.css';
+
+export class Layout extends React.Component {
+    render() {
+        return <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-sm-3'>
+                    <NavMenu />
+                </div>
+                <div className='col-sm-9'>
+                    { this.props.children }
+                </div>
+            </div>
+        </div>;
+    }
+}
