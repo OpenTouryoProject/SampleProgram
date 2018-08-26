01b6122f95ec0cef04cbd1daed22279485dcb55d
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-08-26 19:37:40 +0900

    Added.

---
+++ ASPNET/AuthN_AuthZ/ASPNETIdentity/ASPNETIdentity3Sample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASPNETIdentity3Sample.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASPNETIdentity3Sample.Areas.Identity.IdentityHostingStartup))]
+namespace ASPNETIdentity3Sample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
