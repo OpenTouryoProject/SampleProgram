262497624163164a30549b18197b9d11ebef1bb9
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-11-05 16:33:05 +0900

    tweak.

--- Other/jose_jwt_Sample/jose_jwt_Sample/Program.cs
+++ Other/jose_jwt_Sample/jose_jwt_Sample/Program.cs
@@ -16,8 +16,11 @@ namespace jose_jwt_Sample
             string token = "";
 
             byte[] secretKey = null;
-            CngKey publicKey = null;
-            CngKey privateKey = null;
+
+            CngKey publicKeyOfCng = null;
+            CngKey privateKeyOfCng = null;
+            X509Certificate2 publicKeyX509 = null;
+            X509Certificate2 privateKeyX509 = null;
 
             byte[] x = null;
             byte[] y = null;
@@ -51,6 +54,16 @@ namespace jose_jwt_Sample
             // RS256, RS384, RS512 and PS256, PS384, PS512
             // https://github.com/dvsekhvalnov/jose-jwt#rs--and-ps--family
             // X509Certificate2 x509Certificate2 = new X509Certificate2();
+            string privateX509Path = @"C:\root\files\resource\X509\SHA256RSA.pfx";
+            string publicX509Path = @"C:\root\files\resource\X509\SHA256RSA.cer";
+            privateKeyX509 = new X509Certificate2(privateX509Path, "test",
+                X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+            publicKeyX509 = new X509Certificate2(publicX509Path, "",
+                X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+            token = "";
+            token = Jose.JWT.Encode(payload, privateKeyX509.PrivateKey, JwsAlgorithm.RS256);
+            Debug.WriteLine("JwsAlgorithm.RS256: " + token);
+            Debug.WriteLine("Decoded: " + JWT.Decode(token, publicKeyX509.PublicKey.Key));
 
             // ES- * family
             // ES256, ES384, ES512 ECDSA signatures
@@ -59,11 +72,12 @@ namespace jose_jwt_Sample
             y = new byte[] { 131, 116, 8, 14, 22, 150, 18, 75, 24, 181, 159, 78, 90, 51, 71, 159, 214, 186, 250, 47, 207, 246, 142, 127, 54, 183, 72, 72, 253, 21, 88, 53 };
             d = new byte[] { 42, 148, 231, 48, 225, 196, 166, 201, 23, 190, 229, 199, 20, 39, 226, 70, 209, 148, 29, 70, 125, 14, 174, 66, 9, 198, 80, 251, 95, 107, 98, 206 };
 
-            privateKey = EccKey.New(x, y, d);
+            privateKeyOfCng = EccKey.New(x, y, d);
+            publicKeyOfCng = EccKey.New(x, y);
             token = "";
-            token = JWT.Encode(payload, privateKey, JwsAlgorithm.ES256);
+            token = JWT.Encode(payload, privateKeyOfCng, JwsAlgorithm.ES256);
             Debug.WriteLine("JwsAlgorithm.ES256: " + token);
-            Debug.WriteLine("Decoded: " + JWT.Decode(token, privateKey));
+            Debug.WriteLine("Decoded: " + JWT.Decode(token, publicKeyOfCng));
 
             #endregion
 
@@ -105,11 +119,11 @@ namespace jose_jwt_Sample
             //// https://github.com/dvsekhvalnov/jose-jwt#ecdh-es-and-ecdh-es-with-aes-key-wrap-key-management-family-of-algorithms
             //x = new byte[] { 4, 114, 29, 223, 58, 3, 191, 170, 67, 128, 229, 33, 242, 178, 157, 150, 133, 25, 209, 139, 166, 69, 55, 26, 84, 48, 169, 165, 67, 232, 98, 9 };
             //y = new byte[] { 131, 116, 8, 14, 22, 150, 18, 75, 24, 181, 159, 78, 90, 51, 71, 159, 214, 186, 250, 47, 207, 246, 142, 127, 54, 183, 72, 72, 253, 21, 88, 53 };
-            //publicKey = EccKey.New(x, y, usage: CngKeyUsages.KeyAgreement);
+            //publicKeyOfCng = EccKey.New(x, y, usage: CngKeyUsages.KeyAgreement);
             //token = "";
-            //token = JWT.Encode(payload, publicKey, JweAlgorithm.ECDH_ES, JweEncryption.A256GCM);
+            //token = JWT.Encode(payload, publicKeyOfCng, JweAlgorithm.ECDH_ES, JweEncryption.A256GCM);
             //Debug.WriteLine("JweAlgorithm.ECDH_ES, JweEncryption.A256GCM: " + token);
-            //Debug.WriteLine("Decoded: " + JWT.Decode(token, publicKey));
+            //Debug.WriteLine("Decoded: " + JWT.Decode(token, publicKeyOfCng));
 
             // PBES2 using HMAC SHA with AES Key Wrap key management family of algorithms
             // PBES2-HS256+A128KW, PBES2-HS384+A192KW, PBES2-HS512+A256KW key management
