ab0e760e782230d7dfa04cda940e18fb6fa04aae
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2020-11-19 12:44:39 +0900

    Added the MySparkApp.

---
+++ Azure/DotNet4ApacheSpark/MySparkApp/MySparkApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace MySparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // Create Spark session
+            SparkSession spark =
+                SparkSession
+                    .Builder()
+                    .AppName("word_count_sample")
+                    .GetOrCreate();
+
+            // Create initial DataFrame
+            string filePath = args[0];
+            DataFrame dataFrame = spark.Read().Text(filePath);
+
+            //Count words
+            DataFrame words =
+                dataFrame
+                    .Select(Split(Col("value"), " ").Alias("words"))
+                    .Select(Explode(Col("words")).Alias("word"))
+                    .GroupBy("word")
+                    .Count()
+                    .OrderBy(Col("count").Desc());
+
+            // Display results
+            words.Show();
+
+            // Stop Spark session
+            spark.Stop();
+        }
+    }
+}
