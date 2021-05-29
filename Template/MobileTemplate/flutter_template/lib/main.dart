2abc7416ff1920e2b11d9754f26612cea113912b
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-05-30 03:01:27 +0900

    Added implementation of SBS to flutter_template.

--- Template/MobileTemplate/flutter_template/lib/main.dart
+++ Template/MobileTemplate/flutter_template/lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// Platform呼出
+import 'package:flutter/services.dart';
+
+// WebAPI呼出
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+// AppAuth呼出
+import 'package:flutter_appauth/flutter_appauth.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,7 +40,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -48,6 +60,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  String _display = "hoge";
+
+  // Platform呼出
+  static const platform = const MethodChannel('flutter_template.opentouryo.com/battery');
+
+  // FlutterAppAuth
+  final FlutterAppAuth _appAuth = FlutterAppAuth();
+
+  String? _codeVerifier;
+  String? _authorizationCode;
+  String? _refreshToken;
+  String? _accessToken;
+
+  final String _clientId = 'interactive.public';
+  final String _redirectUrl = 'io.identityserver.demo:/oauthredirect';
+  final String _issuer = 'https://demo.identityserver.io';
+  final String _discoveryUrl =
+      'https://demo.identityserver.io/.well-known/openid-configuration';
+  final List<String> _scopes = <String>[
+    'openid',
+    'profile',
+    'email',
+    'offline_access',
+    'api'
+  ];
+
+  final AuthorizationServiceConfiguration _serviceConfiguration =
+    const AuthorizationServiceConfiguration(
+        'https://demo.identityserver.io/connect/authorize',
+        'https://demo.identityserver.io/connect/token');
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -55,8 +98,113 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      this._display = (this._counter++).toString();
     });
+  }
+
+  void _englishWords() {
+    setState(() {
+      this._display = WordPair.random().asPascalCase;
+    });
+  }
+
+  void _urlLauncher() async {
+    const url = "https://www.osscons.jp/jo5v2ne7n-537/";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not Launch $url';
+    }
+  }
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+    setState(() {
+      this._display = batteryLevel;
+    });
+  }
+
+  Future<void> _getBooks() async {
+    var url =
+    Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['totalItems'];
+      setState(() {
+        this._display = itemCount.toString();
+      });
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  Future<void> _signInWithNoCodeExchange() async {
+    try {
+      final AuthorizationResponse? result
+        = await this._appAuth.authorize(AuthorizationRequest(
+            this._clientId, this._redirectUrl,
+            discoveryUrl: this._discoveryUrl, scopes: this._scopes),
+      );
+      if (result != null) {
+        print("AuthorizationRequest was returned the response.");
+        print("authorizationCode: " + result.authorizationCode!.toString());
+        this._codeVerifier = result.codeVerifier;
+        this._authorizationCode = result.authorizationCode!;
+        await this._exchangeCode();
+      }
+      else {
+        print("AuthorizationResponse is null");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _exchangeCode() async {
+    try {
+      final TokenResponse? result = await this._appAuth.token(TokenRequest(
+          this._clientId, this._redirectUrl,
+          authorizationCode: this._authorizationCode,
+          discoveryUrl: this._discoveryUrl,
+          codeVerifier: this._codeVerifier,
+          scopes: this._scopes));
+      if (result != null) {
+        print("TokenRequest was returned the response.");
+        print("accessToken: " + result.accessToken!.toString());
+        this._accessToken = result.accessToken!;
+        await this._testApi();
+      }
+      else {
+        print("TokenResponse is null");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _testApi() async {
+    final http.Response httpResponse = await http.get(
+        Uri.parse('https://demo.identityserver.io/api/test'),
+        headers: <String, String>{'Authorization': 'Bearer ' + this._accessToken!});
+    setState(() {
+      this._display = httpResponse.statusCode == 200 ?
+        httpResponse.body : httpResponse.statusCode.toString();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
@@ -73,41 +221,84 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+                '$_display',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                child: const Text('EnglishWords Button'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: this._englishWords,
+              ),
+              ElevatedButton(
+                child: const Text('UrlLauncher Button'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: this._urlLauncher,
+              ),
+            ]
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Text('BatteryLevel Button'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: this._getBatteryLevel,
+                ),
+                ElevatedButton(
+                  child: const Text('GetBooks Button'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: this._getBooks,
+                ),
+              ]
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Text('SignIn Button'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: this._signInWithNoCodeExchange,
+                ),
+              ]
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
