6cbff782d022e96ac6073be6ffedaec2b8702c05
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-06-25 16:35:26 +0900

    Added the mySparkStreamingApp.

---
+++ Azure/DotNet4ApacheSpark/mySparkStreamingApp/mySparkStreamingApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Streaming;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace mySparkStreamingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // Default to running on localhost:9999
+            string hostname = "localhost";
+            int port = 9999;
+
+            // User designated their own host and port
+            if (args.Length == 2)
+            {
+                hostname = args[0];
+                port = int.Parse(args[1]);
+            }
+
+            // Create Spark session
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("Streaming example with a UDF")
+                .GetOrCreate();
+
+            // Create initial DataFrame
+            DataFrame lines = spark
+                .ReadStream()
+                .Format("socket")
+                .Option("host", hostname)
+                .Option("port", port)
+                .Load();
+
+            // UDF to produce an array
+            // Array includes:
+            // 1) original string
+            // 2) original string + length of original string
+            Func<Column, Column> udfArray =
+                Udf<string, string[]>((str) => new string[] { str, $"{str} {str.Length}" });
+
+            // Explode array to rows
+            DataFrame arrayDF = lines.Select(Explode(udfArray(lines["value"])));
+
+            // Process and display each incoming line
+            StreamingQuery query = arrayDF
+                .WriteStream()
+                .Format("console")
+                .Start();
+
+            query.AwaitTermination();
+        }
+    }
+}
